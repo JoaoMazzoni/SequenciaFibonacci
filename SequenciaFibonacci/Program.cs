436ceaf7d6758fab159553d64679bf39d8238845
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-23 09:39:53 -0300

    Criação de nova lógica. Mudança de variaveis.

--- SequenciaFibonacci/Program.cs
+++ SequenciaFibonacci/Program.cs
@@ -1,44 +1,26 @@
 ﻿
-long numero;
-long n1 = 0, n2 = 1;
-long n3;
+long numeroPos;
+long penultimo = -1, ultimo = 0,  atual;
 
 do
 {
-    Console.Write("Digite um número: ");
-    numero = long.Parse(Console.ReadLine());
-    if (numero <= 0 )
+    Console.Write("Digite o número da posição limite: ");
+    numeroPos = long.Parse(Console.ReadLine());
+    if (numeroPos <= 0 )
     {
         Console.WriteLine("\nO número não pode ser menor ou igual a zero.");
     }
-} while (numero <= 0);
+} while (numeroPos <= 0);
 
 Console.WriteLine("\n Sequencia Fibonacci: ");
 
-if (numero == 1)
+for (int i = 0; i < numeroPos; i++)
 {
-    Console.WriteLine(n1);
+    atual = penultimo + ultimo;
+    Console.WriteLine(atual);
+    penultimo = ultimo;
+    ultimo = atual;
 }
 
-else if (numero == 2)
-{
-    Console.WriteLine(n1);
-    Console.WriteLine(n2);
-}
-
-else
-{
-    Console.WriteLine(n1);
-    Console.WriteLine(n2);
-
-    for (int i = 0; i < numero - 2; i++)
-    {
-        n3 = n1 + n2;
-        n1 = n2;
-        n2 = n3;
-
-        Console.WriteLine(n3);
-    }
-}
 Console.ReadKey();
 
