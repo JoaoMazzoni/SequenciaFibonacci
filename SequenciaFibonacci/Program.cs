790620b16b6d13007ed540d3b2d2e2d2d2b5fa40
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-23 09:32:30 -0300

    Adicionando condições de tratamento.

--- SequenciaFibonacci/Program.cs
+++ SequenciaFibonacci/Program.cs
@@ -1,30 +1,44 @@
 ﻿
 long numero;
-long n1, n2;
+long n1 = 0, n2 = 1;
 long n3;
 
 do
 {
-    Console.WriteLine("Digite um número: ");
+    Console.Write("Digite um número: ");
     numero = long.Parse(Console.ReadLine());
     if (numero <= 0 )
     {
-        Console.WriteLine("O número não ser menor ou igual a zero.");
+        Console.WriteLine("\nO número não pode ser menor ou igual a zero.");
     }
 } while (numero <= 0);
 
 Console.WriteLine("\n Sequencia Fibonacci: ");
 
-n1 = 0;
-n2 = 1;
-Console.WriteLine(n1);
-Console.WriteLine(n2);
-
-for (int i = 0; i < numero - 2; i++)
+if (numero == 1)
 {
-    n3 = n1 + n2;
-    n1 = n2;
-    n2 = n3;
-
-    Console.WriteLine(n3);
+    Console.WriteLine(n1);
 }
+
+else if (numero == 2)
+{
+    Console.WriteLine(n1);
+    Console.WriteLine(n2);
+}
+
+else
+{
+    Console.WriteLine(n1);
+    Console.WriteLine(n2);
+
+    for (int i = 0; i < numero - 2; i++)
+    {
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+
+        Console.WriteLine(n3);
+    }
+}
+Console.ReadKey();
+
